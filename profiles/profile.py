e3c4e1b05d49b0af3f82c15420e762fde5efe124
iva <ivalulyanov1@gmail.com>
2024-09-23 13:56:44 +0300

    update in profile, new modules and new features

--- profiles/profile.py
+++ profiles/profile.py
@@ -19,6 +19,60 @@ async def read_profile(call: CallbackQuery) -> None:
     await call.message.edit_text(f"🗓 <b><i>Профиль студента</i></b> <i>{await fullname(call.message.chat)}</i> 🗓", reply_markup=InlineKeyboardMarkup(inline_keyboard=[
             [InlineKeyboardButton(text="🖥 СДО", web_app=WebAppInfo(url="https://online-edu.mirea.ru/login/"))],
             [InlineKeyboardButton(text="📸 Сканер QR ", web_app=WebAppInfo(url="https://attendance-app.mirea.ru/selfapprove"))],
+            [InlineKeyboardButton(text="📚 Предметы", callback_data="subjects")],
+            [InlineKeyboardButton(text="🔗 Темы для курсовой", web_app=WebAppInfo(url="https://clck.ru/3DMcY9"))],
+            [InlineKeyboardButton(text="Назад", callback_data="back")]
+    ]))
+
+
+@router.callback_query(F.data == 'subjects')
+async def view_subjects(call: CallbackQuery):
+    await call.message.edit_text("Предметы", reply_markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="📕 САПР Констр. РС", callback_data="sapr")],
+            [InlineKeyboardButton(text="📗 Социальная пед. и психол.", callback_data="pedagogy")],
+            [InlineKeyboardButton(text="Назад", callback_data="back_for_subjects")]
+        ]
+    ))
+
+
+@router.callback_query(F.data == "pedagogy")
+async def pedagogy(call: CallbackQuery):
+      await call.message.edit_text("Социяльная психология и педагогика", reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                  [InlineKeyboardButton(text="Темы презентаций", web_app=WebAppInfo(url="https://clck.ru/3DTVVa"))],
+                  [InlineKeyboardButton(text="Назад", callback_data="back_to_subject")]
+            ]
+      ))
+
+
+@router.callback_query(F.data == "sapr")
+async def capr(call: CallbackQuery):
+     await call.message.edit_text("САПР конструкций радиоэлектронных средств", reply_markup=InlineKeyboardMarkup(
+          inline_keyboard=[
+               [InlineKeyboardButton(text="✏️ Темы для курсовой", web_app=WebAppInfo(url="https://clck.ru/3DMcY9"))],
+               [InlineKeyboardButton(text="Назад", callback_data="back_to_subject")]
+          ]
+     ))
+
+
+@router.callback_query(F.data == "back_to_subject")
+async def back_to_subjects(call: CallbackQuery):
+         await call.message.edit_text("Предметы", reply_markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="📕 САПР Констр. РС", callback_data="sapr")],
+            [InlineKeyboardButton(text="📗 Социальная пед. и психол.")],
+            [InlineKeyboardButton(text="Назад", callback_data="back_for_subjects")]
+        ]
+    ))
+
+
+@router.callback_query(F.data == "back_for_subjects")
+async def back_for_subjects(call: CallbackQuery):
+        await call.message.edit_text(f"🗓 <b><i>Профиль студента</i></b> <i>{await fullname(call.message.chat)}</i> 🗓", reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="🖥 СДО", web_app=WebAppInfo(url="https://online-edu.mirea.ru/login/"))],
+            [InlineKeyboardButton(text="📸 Сканер QR ", web_app=WebAppInfo(url="https://attendance-app.mirea.ru/selfapprove"))],
+            [InlineKeyboardButton(text="📚 Предметы", callback_data="subjects")],
             [InlineKeyboardButton(text="🔗 Темы для курсовой", web_app=WebAppInfo(url="https://clck.ru/3DMcY9"))],
             [InlineKeyboardButton(text="Назад", callback_data="back")]
     ]))
