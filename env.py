bfc1ad01d644d90904b19b0c90c2fa96d02bc946
iva <ivalulyanov1@gmail.com>
2024-09-28 01:36:28 +0300

    add new url

--- env.py
+++ env.py
@@ -9,3 +9,4 @@ class Telegram:
         self.token = os.getenv('BOT_TOKEN')
         self.url = os.getenv('URL')
         self.admins = os.getenv('ADMINS')
+        self.url_jurnal = os.getenv('URL_JURNAL')
